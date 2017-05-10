680410ab40402fd92ad9bd1b65ff048eda5f5e7c
rolita1007 <rolita1007@gmail.com>
2017-05-10 12:34:09 -0400

    working progress

--- connect.js
+++ connect.js
@@ -1,14 +1,21 @@
-$( document ).ready (function () {
+$( document ).ready (function ()  {
+	
 
+	function  gameSetUp(){
+		for (var i = 0; i < 42; i++) {
+			var makeSquares = $('#board');
+			makeSquares.append('<div class="square"></div>'); 
+           
+        }
 
-
-
-
-
-
-
-
-
-
-
+	};
+gameSetUp();	
 });
+
+
+
+
+
+
+
+
