15ef441c668b72d869cc8ee8bad7dbb096fea353
rolita1007 <rolita1007@gmail.com>
2017-05-11 12:47:19 -0400

    click effect

--- connect.js
+++ connect.js
@@ -1,20 +1,94 @@
-$( document ).ready (function ()  {
+// 
+$(document).ready(function ()  {
 	
 
 	function  gameSetUp(){
 		for (var i = 0; i < 42; i++) {
 			var makeSquares = $('#board');
-			makeSquares.append('<div class="square"></div>'); 
-           
-        }
+			makeSquares.append($('<div/>', {
+              id: 'littleSquares' + i,
+              class: 'square'
+            }));
+
+         
+
+         }
 
 	};
-gameSetUp();	
+	
+	gameSetUp();
+
+	//  For start button, instead of line 12
+	// create a start button in html, and have it call gameSetUp() when clicked
+
+	// 
 
 
 
+	var currentPlayer = 1;
+
+
+
+
+
+
+
+	$('#board').on('click', function (event){
+		$(event.target).addClass(`player${currentPlayer}`);
+			if (currentPlayer === 1){ // write an and(&&) statement that checks to see if the space already has a class of player 2 
+				currentPlayer = 2;
+				// alert() event.target.id
+			} else if (currentPlayer === 2){ // write an and(&&) statement that checks to see if the space already has a class of player 1
+				currentPlayer = 1;
+				// alert() event.target.id
+			}
+	});
+
+	var x = x;
+	var y = y;
+
+function dropPiece(x, player){
+ 		for (var i = 5; i >= 0; i--){
+ 			if (!board[i][x]) {
+	 			board[i][x] = player;
+	 			return;
+ 			}
+ 		}
+ 	}
+ 	
+ 	var board = [
+		[0,0,0,0,0,0,0],
+	    [0,0,0,0,0,0,0],
+ 	 	[0,0,0,0,0,0,0],
+ 	    [0,0,0,0,0,0,0],
+ 	    [0,0,0,0,0,0,0],
+ 	    [0,0,0,0,0,0,0]
+ 	];
+
+
+
+
+	// var board = [
+	// 	[0,0,0,0,0,0,0],
+	//     [0,0,0,0,0,0,0],
+ // 	 	[0,0,0,0,0,0,0],
+ // 	    [0,0,0,0,0,0,0],
+ // 	    [0,0,0,0,0,0,0],
+ // 	    [0,0,0,0,0,0,0]
+ // 	];
+
+ // 	function dropPiece(x, player){
+ // 		for (var y = 5; y >= 0; y--){
+ // 			if (!board [i][x]) {
+ // 			board [i][x] = player;
+ // 			}
+ // 		};
+ // 	};
+
+ console.table(board);
+
+		
 });
-
 
 
 
